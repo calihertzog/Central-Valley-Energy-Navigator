ec3ee0174313125dad410885d1e4982e89145bf9
calihertzog <127066402+calihertzog@users.noreply.github.com>
2026-06-02 19:24:13 -0700

    update write-in background color

--- frontend/src/Survey.jsx
+++ frontend/src/Survey.jsx
@@ -178,7 +178,8 @@ export default function Survey({ onCancel, onComplete }) {
               width: '100%', 
               maxWidth: '300px',
               borderRadius: '8px',
-              border: '2px solid #244D73'
+              border: '2px solid #244D73',
+              backgroundColor: 'white'
             }}
           />
         </div>
